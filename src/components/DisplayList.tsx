ee434490eaf4161c9415cfe28528f71f57bb8869
Szymon S <fide.simon@gmail.com>
2020-01-06 00:53:16 +0100

    added template for DisplayList component

---
+++ src/components/DisplayList.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default class DisplayList extends React.Component<{},{}>{
+
+    render(){
+        return (
+            <>
+            </>
+        );
+    }
+}
