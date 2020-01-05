18a0c8c793d7c6f59c8ae286dc92ad1af1f6e15b
Szymon S <fide.simon@gmail.com>
2020-01-06 00:50:43 +0100

    added AddItem component

---
+++ src/components/AddItem.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+
+interface AddItemProps {
+
+}
+
+interface AddItemState{
+
+}
+
+export default class AddItem extends React.Component<AddItemProps, AddItemState>{
+
+    render(){
+        return (
+            <>
+            </>
+        )
+    }
+}
