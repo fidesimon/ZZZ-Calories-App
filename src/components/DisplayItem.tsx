9e42e7c40d8c2ed5febe68447bc021405278da1f
Szymon S <fide.simon@gmail.com>
2020-01-06 01:10:04 +0100

    updated template for DisplayItem component to display all data

--- src/components/DisplayItem.tsx
+++ src/components/DisplayItem.tsx
@@ -7,9 +7,15 @@ interface DisplayItemProps {
 
 export default class DisplayItem extends React.Component<DisplayItemProps, {}>{
 
-    render(){
+    render() {
         return (
             <>
+                <td>{this.props.itemData.name}</td>
+                <td>{this.props.itemData.grams}</td>
+                <td>{this.props.itemData.calories}</td>
+                <td>{this.props.itemData.proteins}</td>
+                <td>{this.props.itemData.carbs}</td>
+                <td>{this.props.itemData.fat}</td>
             </>
         )
     }
