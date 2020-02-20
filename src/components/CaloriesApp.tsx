7c4a2d5752bb464d89f259bfda3104fab423c088
Szymon S <fide.simon@gmail.com>
2020-02-20 23:35:58 +0100

    removed IItemData.tsx which is no longer used

--- src/components/CaloriesApp.tsx
+++ src/components/CaloriesApp.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import IItemData from './IItemData';
 import { Summary } from './Summary';
 import { AddItem } from './AddItem';
 import DisplayList from './DisplayList';
--- src/components/IItemData.tsx
+++
@@ -1,8 +0,0 @@
-export default interface IItemData{
-    name: string,
-    qty: number,
-    calories: number,
-    proteins: number,
-    carbs: number,
-    fat: number
-}
