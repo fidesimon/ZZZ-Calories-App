a2feaea3b3c5d83f0b5847041d5a12eb9e32b460
Szymon S <fide.simon@gmail.com>
2020-03-21 22:40:34 +0100

    Updated summary table to Material UI

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,4 +1,11 @@
-import * as React from 'react'
+import * as React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 interface SummaryProps {
     calories: number;
@@ -7,14 +14,41 @@ interface SummaryProps {
     fat: number;
 }
 
-export const Summary: React.FC<SummaryProps> = (props) => {
+const CustomTableCell = withStyles(theme => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
 
+export const Summary: React.FC<SummaryProps> = (props) => {
     return (
-        <ul>
-            <li>Calories: {props.calories}</li>
-            <li>Proteins: {props.proteins}</li>
-            <li>Carbs: {props.carbs}</li>
-            <li>Fat: {props.fat}</li>
-        </ul>
+        <Paper>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <CustomTableCell>Dessert (100g serving)</CustomTableCell>
+                        <CustomTableCell>Calories</CustomTableCell>
+                        <CustomTableCell>Fat (g)</CustomTableCell>
+                        <CustomTableCell>Carbs (g)</CustomTableCell>
+                        <CustomTableCell>Protein (g)</CustomTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow key="sum">
+                        <TableCell component="th" scope="row">
+                            Sum
+                                </TableCell>
+                        <TableCell>{props.calories}</TableCell>
+                        <TableCell>{props.fat}</TableCell>
+                        <TableCell>{props.carbs}</TableCell>
+                        <TableCell>{props.proteins}</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </Paper>
     );
 }
